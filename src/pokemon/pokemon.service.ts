68c2cbfdeb5602f63625918da872d30117e5cdab
Lucas Previato <lucaspreviato@live.com>
2022-09-10 15:49:38 -0300

    fix(prisma): fix PrismaClient import

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class PokemonService {
   constructor( private readonly prisma : PrismaService){
-    
+
   }
   create(createPokemonDto: CreatePokemonDto) {
     return 'This action adds a new pokemon';
